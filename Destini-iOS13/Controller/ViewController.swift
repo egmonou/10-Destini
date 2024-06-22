14a4f7a3b3a57240d27d28428ffd3b170b5228f1
Nour <mohamednour25@gmail.com>
2024-06-23 01:12:04 +0300

    01

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,10 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+    
+    @IBAction func userChoice(_ sender: UIButton) {
+        let choice = sender.currentTitle!
+        storyBrain.updateTitle(userAnswer: choice)
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getchoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getchoice2(), for: .normal)
+    }
+    
+
 
 
 }
